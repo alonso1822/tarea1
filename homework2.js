750ca5220e825161c18063640da8a9f48abb60f8
AlonsoA <alonso.aguirre.e.3@gmail.com>
2022-11-07 16:45:35 -0500

    segundo ejercicio

---
+++ homework2.js
@@ -0,0 +1,16 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos //
+
+let nombre = "alonso";
+
+let edad = 28;
+
+const eresDesarrollador = true;
+
+const fechadeNacimiento = new Date("3 November 1993");
+
+const libroFavorito = {
+    titulo : "Moby-Dick",
+    autor : "Herman Melville",
+    fecha : "18 de octubre de 1851",
+    url : "https://es.wikipedia.org/wiki/Moby_Dick"
+}
